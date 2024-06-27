b5c2a7eb22f26164ec9571e2eb19f6c3c150a8b3
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-27 22:53:40 +0530

    else

--- Exercise Files/02_02/begin/if.sh
+++ Exercise Files/02_02/begin/if.sh
@@ -4,6 +4,8 @@ COLOR=$1
 if [ $COLOR = "blue" ]
 then
   echo "The color is blue"
+else 
+  echo "The color is NOT blue"
 fi
 
 USER_GUESS=$2
@@ -12,5 +14,6 @@ COMPUTER=50
 if [ $USER_GUESS -lt $COMPUTER ]
 then
   echo "You're too low"
-
+else 
+  echo "You're equal or too high"
 fi
